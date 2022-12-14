01d98fa6ad7d7392330a315fb06fad2ca12044c9
Jenn Guneratne <jenn.guneratne@gmail.com>
2022-12-14 11:27:50 -0500

    Create simple operator functions

---
+++ script.js
@@ -0,0 +1,11 @@
+function add(addNum1, addNum2) {
+    return sum = addNum1 + addNum2;
+}
+
+function subtract(subNum1, subNum2) {
+    return diff = subNum1 - subNum2;
+};
+
+function multiply(multNum1, multNum2) {
+    return prod = multNum1 * multNum2;
+};
