dcd3d8dcaf5167534fd751989139310a27b0644a
Jenn Guneratne <jenn.guneratne@gmail.com>
2022-12-14 14:03:22 -0500

    Add division operator

--- script.js
+++ script.js
@@ -9,3 +9,7 @@ function subtract(subNum1, subNum2) {
 function multiply(multNum1, multNum2) {
     return prod = multNum1 * multNum2;
 };
+
+function divide(divNum1, divNum2) {
+    return quot = divNum1 / divNum2;
+};
