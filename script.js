098b36a1d9300a73bdad288c9d5d40ce54034c57
jguneratne <59374962+jguneratne@users.noreply.github.com>
2023-01-05 08:11:48 -0500

    Attempt to work on operation and calculation methods

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
 
-// Global Variables: 
+// Global Variables:
 const buttons = document.querySelectorAll('.btns'); // Need to use forEach to add event listener, because cannot add it to node list returned by querySelectorAll
 const screen = document.querySelector('.screen');
 
-// Calculator Object: 
+// Calculator Object:
 
 const calculator = {
-    displayValue: '0', 
-    firstOperand: '', 
-    nextOperand: '', 
+    displayValue: '0',
+    firstOperand: '',
+    nextOperand: '',
     result: '',
     operator: undefined,
 
@@ -22,11 +22,18 @@ const calculator = {
 
 
     appendDigit: function(num) {
-        if(this.displayValue === '0') {
-            this.displayValue = num;
+        
+        if(this.operator !== undefined) {
+            this.displayValue = this.nextOperand
+            console.log(this.nextOperand);
         } else {
-            this.displayValue = this.displayValue + num;
-        };
+            //Logic to append numbers as they're typed
+            if(this.displayValue === '0') {
+                this.displayValue = num;
+            } else {
+                this.displayValue = this.displayValue + num;
+            };
+        }
         
         return this;
     },
@@ -46,46 +53,58 @@ const calculator = {
         } else {
             this.displayValue = this.displayValue.toString().slice(0, -1);
         }
-        
+
         return this;
     },
 
 
     operation: function(op) {
-        if(this.firstOperand === '' || this.firstOperand === NaN || this.nextOperand === '' || this.nextOperand === NaN || this.operator === undefined) {
-            return
+
+        this.operator = op;
+
+        if(this.operator = undefined) {
+            this.firstOperand += this.displayValue;
+
+        } else {
+            this.nextOperand += this.displayValue;
+
         }
 
-        this.operation = op;
 
-        if(this.operation = undefined) {
-            this.firstOperand = parseFloat(this.displayValue);
-                console.log(this.firstOperand)
-        } else if(this.firstOperand !== '' && this.operator !== undefined) {
-            this.nextOperand = parseFloat(this.displayValue);
-                console.log(this.nextOperand);
-        }
-
-        if (op === '+') {
-            return this.firstOperand + this.nextOperand;
-        } else if (op === '-') {
-            return this.firstOperand - this.nextOperand;
-        } else if (op === '*') {
-            return this.firstOperand * this.nextOperand;
-        } else if (op === '/') {
-            return this.firstOperand / this.nextOperand;
-        }
-    
-        return this; 
+        return this;
     },
 
-    
+
     calculation: function(nextOp) {
-        if(nextOp === '=') {
-            this.displayValue = toString(this.result);
-        } else if (nextOp === '+' || nextOp === '-' || nextOp === '*' || nextOp === '/') {
-            (this.firstOperand = this.result) && (this.nextOperand = '');
+        if(nextOp !== '=') {
+            this.operator = nextOp
+
+            console.log(this.firstOperand);
+            console.log(this.operator);
+
+        } else {
+            console.log(this.nextOperand);
+
+            switch (this.operator) {
+                case "+":
+                console.log(parseInt(this.firstOperand) + parseInt(this.nextOperand));
+                break;
+
+                case "-":
+                console.log(parseInt(this.firstOperand) - parseInt(this.nextOperand));
+                break;
+
+                case "*":
+                console.log(parseInt(this.firstOperand) * parseInt(this.nextOperand));
+                break;
+
+                case "/":
+                console.log(parseInt(this.firstOperand) / parseInt(this.nextOperand));
+                break;
+            }
         }
+
+
         return this;
     },
 
@@ -110,7 +129,7 @@ buttons.forEach(button => {
         if(!e.target.matches('.btns')) {
             return;
         } else if (e.target.matches('.btn-num')) {
-            calculator.appendDigit(target.dataset.value).updateScreen();   
+            calculator.appendDigit(target.dataset.value).updateScreen();
         } else if (e.target.matches('.btn-dec')) {
             calculator.addDecimal(target.dataset.value).updateScreen();
         } else if (e.target.matches('.btn-op')) {
@@ -120,19 +139,19 @@ buttons.forEach(button => {
         } else if (e.target.matches('.btn-undo')) {
             calculator.undo().updateScreen();
         }
-        
+
     });
 });
 
 document.addEventListener('keydown', function(e) {
         if((e.key === '1') || (e.key === '2') || (e.key === '3') || (e.key === '4') || (e.key === '5') || (e.key === '6') || (e.key === '7') || (e.key === '8') || (e.key === '9') || (e.key === '0')) {
-            calculator.appendDigit(e.key).updateScreen();   
+            calculator.appendDigit(e.key).updateScreen();
         } else if (e.key === '.') {
             calculator.addDecimal(e.key).updateScreen();
         } else if ((e.key === '+') || (e.key === '-') || (e.key === '*') || (e.key === '/') || (e.key === '=')) {
             calculator.operation(e.key).calculation(e.key).updateScreen();
         } else if ((e.key === 'Enter')) {
-            console.log(e.key);
+            // calculator.operation(e.target.matches('btn-op'.calculation(e.target.matches('btn-op'));
         } else if ((e.key === 'Backspace')) {
             calculator.undo().updateScreen();
         } else if ((e.key === 'd')) {
@@ -145,4 +164,4 @@ document.addEventListener('keydown', function(e) {
 
 // Method Calls
 
-calculator.updateScreen(); 
+calculator.updateScreen();
